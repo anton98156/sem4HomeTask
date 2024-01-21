14c7f4451b65ae4731bc1f10d9d71c16bf0410b5
Антон Кравцов <antonkravcov@MacBook-Air-Anton.local>
2024-01-21 16:40:45 +0300

    Done

---
+++ src/main/java/com/example/sem4HomeTask/controllers/StartPageController.java
@@ -0,0 +1,50 @@
+package com.example.sem4HomeTask.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class StartPageController {
+
+    @GetMapping("/")
+    public String getIndexPage(Model model) {
+        model.addAttribute("pageTitle", "Добро пожаловать!");
+        model.addAttribute("message", "Привет, это страница с использованием Thymeleaf");
+        return "index";
+    }
+
+    @PostMapping("/submitForm")
+    public String submitForm(@RequestParam("name") String name, Model model) {
+        model.addAttribute("pageTitle", "Результат");
+        model.addAttribute("message", "Привет, " + name + "!");
+        return "index";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// package com.example.sem4HomeTask.controllers;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+// @Controller
+// public class GreetingController {
+//     @RequestMapping("/")
+//     @ResponseBody
+//     public String greeting() {
+//         return "Привет, мир!";
+//     }
+// }
