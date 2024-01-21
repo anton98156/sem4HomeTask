75057f7d646c2c6497fcec0e0b68873d303ec1fa
Антон Кравцов <antonkravcov@MacBook-Air-Anton.local>
2024-01-21 13:50:41 +0300

    Initital commit

---
+++ src/test/java/com/example/sem4HomeTask/Sem4HomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem4HomeTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4HomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
